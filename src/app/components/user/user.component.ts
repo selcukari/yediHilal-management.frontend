2c1adefb149271aeb0503538c5972d1eeb02c4e8
selcuk.ari <selcukari51@gmail.com>
2025-07-28 01:20:13 +0300

    user edit review

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { IconFieldModule } from 'primeng/iconfield';
+import { FloatLabel } from 'primeng/floatlabel';
+import { FormsModule } from '@angular/forms';
+import { MessageModule } from 'primeng/message';
+import { InputIconModule } from 'primeng/inputicon';
 import { Dialog } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
@@ -8,7 +13,7 @@ import { AvatarModule } from 'primeng/avatar';
 @Component({
   selector: 'app-component-user',
   standalone: true,
-  imports: [Dialog, ButtonModule, InputTextModule, AvatarModule],
+  imports: [Dialog, MessageModule, ButtonModule, FormsModule, FloatLabel, IconFieldModule, InputIconModule, InputTextModule, AvatarModule],
   templateUrl: './user.component.html',
 })
 
@@ -16,9 +21,38 @@ export class UserComponent {
   visible: boolean = false;
   userData: any;
 
+
+  ngOnInit() {
+    if (!this.userData) {
+      this.userData = this.defaultUserData();
+    }
+  }
   addOrEdit(newValue: any) {
   this.visible = true;
-  this.userData = newValue || {};
+  this.userData = newValue || this.defaultUserData();
   console.log('New value:', newValue);
+  }
+
+  defaultUserData(): any {
+    return {
+      id: null,
+      fullName: "",
+      isActive: true,
+      countryId: null,
+      areaId: null,
+      provinceId: null,
+      districtId: null,
+      identificationNumber: null,
+      telephone: null,
+      email: null,
+      dateOfBirth: null
+    };
+  }
+
+   async onSubmit(form: any) {
+    console.log('Form submitted with value:', this.userData);
+    console.log('Form validity:', form.valid);
+    if (form.valid) {
+    }
   }
 }
